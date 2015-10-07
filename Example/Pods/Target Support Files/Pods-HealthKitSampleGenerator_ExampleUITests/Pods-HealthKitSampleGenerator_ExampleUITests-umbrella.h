8e53c35a812a9f9eb410bf3d88049b1b81ed41db
Michael Seemann <seemann@mseemann.de>
2015-10-07 14:20:22 +0200

    added testframeworks quick and nimble

---
+++ Example/Pods/Target Support Files/Pods-HealthKitSampleGenerator_ExampleUITests/Pods-HealthKitSampleGenerator_ExampleUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HealthKitSampleGenerator_ExampleUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HealthKitSampleGenerator_ExampleUITestsVersionString[];
+
--- Example/Tests/JsonWriterTest.swift
+++ Example/Tests/JsonWriterTest.swift
@@ -7,61 +7,62 @@
 //
 
 import XCTest
+import Quick
+import Nimble
 import HealthKitSampleGenerator
 
-class JsonWriterTest: XCTestCase {
+class JsonWriterTest: QuickSpec {
     
-    func getStringFormStream(stream: NSOutputStream) -> String {
+    override func spec() {
+        it("should write a simple array as json"){
+            let stream = NSOutputStream.outputStreamToMemory()
+            
+            let jsonWriter = JsonWriter(outputStream: stream)
+            
+            jsonWriter.writeArrayStart()
+            jsonWriter.writeArrayEnd()
+            
+            let jsonString = self.getStringFormStream(stream)
+            
+            expect(jsonString).to(equal("[]"))
+        }
+        
+        it("should write an json object within an array") {
+            let stream = NSOutputStream.outputStreamToMemory()
+            
+            let jsonWriter = JsonWriter(outputStream: stream)
+            
+            jsonWriter.writeArrayStart()
+            try! jsonWriter.writeObject(["a":"b"])
+            jsonWriter.writeArrayEnd()
+            
+            let jsonString = self.getStringFormStream(stream)
+            
+            expect(jsonString).to(equal("[{\n  \"a\" : \"b\"\n}]"))
+        }
+        
+        it("should write an json array with 2 objects"){
+            let stream = NSOutputStream.outputStreamToMemory()
+            
+            let jsonWriter = JsonWriter(outputStream: stream)
+            
+            jsonWriter.writeArrayStart()
+            try! jsonWriter.writeObject(["a":"b"])
+            try! jsonWriter.writeObject(["c":"d"])
+            jsonWriter.writeArrayEnd()
+            
+            let jsonString = self.getStringFormStream(stream)
+            
+            expect(jsonString).to(equal( "[{\n  \"a\" : \"b\"\n},\r\n{\n  \"c\" : \"d\"\n}]"))
+        }
+    }
+    
+    internal func getStringFormStream(stream: NSOutputStream) -> String {
+         stream.close()
         let data = stream.propertyForKey(NSStreamDataWrittenToMemoryStreamKey)
         
         return NSString(data: data as! NSData, encoding: NSUTF8StringEncoding) as! String
     }
     
-    func testWriteSimpleArray() {
-        let stream = NSOutputStream.outputStreamToMemory()
-        
-        let jsonWriter = JsonWriter(outputStream: stream)
-        
-        jsonWriter.writeArrayStart()
-        jsonWriter.writeArrayEnd()
-        
-        stream.close()
-        
-        let jsonString = getStringFormStream(stream)
-        
-        XCTAssertEqual(jsonString, "[]")
-    }
-    
-    func testWriteOneObject() {
-        let stream = NSOutputStream.outputStreamToMemory()
-        
-        let jsonWriter = JsonWriter(outputStream: stream)
-        
-        jsonWriter.writeArrayStart()
-        try! jsonWriter.writeObject(["a":"b"])
-        jsonWriter.writeArrayEnd()
-        
-        stream.close()
-        
-        let jsonString = getStringFormStream(stream)
-        
-        XCTAssertEqual(jsonString, "[{\n  \"a\" : \"b\"\n}]")
-    }
-    
-    func testWriteTwoObject() {
-        let stream = NSOutputStream.outputStreamToMemory()
-        
-        let jsonWriter = JsonWriter(outputStream: stream)
-        
-        jsonWriter.writeArrayStart()
-        try! jsonWriter.writeObject(["a":"b"])
-        try! jsonWriter.writeObject(["c":"d"])
-        jsonWriter.writeArrayEnd()
-        
-        stream.close()
-        
-        let jsonString = getStringFormStream(stream)
-        
-        XCTAssertEqual(jsonString, "[{\n  \"a\" : \"b\"\n},\r\n{\n  \"c\" : \"d\"\n}]")
-    }
+
 }
