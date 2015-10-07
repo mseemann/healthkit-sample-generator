88a1d36275d93cd788efdbf4cb690c896d13b1e7
Michael Seemann <seemann@mseemann.de>
2015-10-07 21:25:41 +0200

    ui tests removed

--- Example/Pods/Target Support Files/Pods-HealthKitSampleGenerator_ExampleUITests/Pods-HealthKitSampleGenerator_ExampleUITests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_HealthKitSampleGenerator_ExampleUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_HealthKitSampleGenerator_ExampleUITestsVersionString[];
-
